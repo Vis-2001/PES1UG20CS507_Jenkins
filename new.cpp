2d42e927e5fb1850e658e7cc51ca7e0cf3a82bb0
Vis-2001 <79145275+Vis-2001@users.noreply.github.com>
2023-02-19 15:35:39 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run\n";
+  cout<<"I am making change to society by sleeping and eating\n";
+  return 0;
+}
